ada3e4aff73a51f48013a753b65be296459d5467
Amanvir <amanvirb@gmail.com>
2024-02-01 15:41:07 +0100

    SearchProduct Updated

---
+++ Application/Dto/ProductSearchParamsDto.cs
@@ -0,0 +1,10 @@
+﻿namespace Application.Dto
+{
+    public class ProductSearchParamsDto
+    {
+        public string SerialNo { get; set; }
+        public string ProductName { get; set; }
+        public string Location { get; set; }
+
+    }
+}
--- Application/Products/ProductList.cs
+++ Application/Products/ProductList.cs
@@ -3,20 +3,23 @@ public class ProductList
 {
     public class Query : IRequest<Result<List<ProductDto>>>
     {
-        public class Handler(DataContext context, IMapper mapper) : IRequestHandler<Query, Result<List<ProductDto>>>
+        public ProductSearchParamsDto Search { get; set; }
+    }
+    public class Handler(DataContext context, IMapper mapper) : IRequestHandler<Query, Result<List<ProductDto>>>
+    {
+        private readonly DataContext _context = context;
+        private readonly IMapper _mapper = mapper;
+
+        public async Task<Result<List<ProductDto>>> Handle(Query request, CancellationToken ct)
         {
-            private readonly DataContext _context = context;
-            private readonly IMapper _mapper = mapper;
+            var productList = await _context.Products
+                .Include(x => x.ProductNumber)
+                .Include(x => x.Location)
+                .ProjectTo<ProductDto>(_mapper.ConfigurationProvider)
+                .ToListAsync(ct);
 
-            public async Task<Result<List<ProductDto>>> Handle(Query request, CancellationToken ct)
-            {
-                var productList = await _context.Products
-                    .Include(x => x.Location)
-                    .ProjectTo<ProductDto>(_mapper.ConfigurationProvider)
-                    .ToListAsync(ct);
-
-                return Result<List<ProductDto>>.Success(productList);
-            }
+            return Result<List<ProductDto>>.Success(productList);
         }
     }
+
 }
